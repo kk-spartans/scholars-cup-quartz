2ef4d88b6e97274e59586fd8e593e816211bcc8c
kk-spartans <kcube.jan+github@gmail.com>
2025-08-30 09:15:34 +0530

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "kk-spartans.github.io/scholars-cup-quartz",
+    baseUrl: "kk-spartans.github.io/scholars-cup-quartz/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
